303f75b5998b5d52c9b349c1368f791ca342915f
Saketh Pabolu <51687055+spabolu@users.noreply.github.com>
2023-03-13 11:35:57 -0700

    added theme toggle animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
+import "@theme-toggles/react/css/Expand.css";
+import { Expand } from "@theme-toggles/react";
 
 import Footer from "./components/Footer";
 import Intro from "./components/Intro";
@@ -7,67 +10,27 @@ import Portfolio from "./components/Portfolio";
 import Timeline from "./components/Timeline";
 
 function App() {
-  const [theme, setTheme] = useState(null);
+  const [theme, setTheme] = useState(false);
 
   useEffect(() => {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
+    setTheme(window.matchMedia("(prefers-color-scheme: dark)").matches);
   }, []);
 
-  const handleThemeSwitch = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
-
   useEffect(() => {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
+    document.documentElement.classList.toggle("dark", theme);
   }, [theme]);
-
-  const sun = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="w-6 h-6"
-    >
-      <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
-    </svg>
-  );
-
-  const moon = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        fillRule="evenodd"
-        d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
 
   return (
     <>
-      <button
-        type="button"
+      <Expand
         aria-label="toggle-theme"
-        onClick={handleThemeSwitch}
-        className="fixed md:p-2 p-1 z-10 md:right-10 right-5  top-4 bg-violet-300 dark:bg-orange-300 text-lg rounded-md"
-      >
-        {theme === "dark" ? sun : moon}
-      </button>
+        className="fixed p-2 z-10 md:right-10 right-5 top-4 bg-violet-300 dark:bg-orange-300 text-lg rounded-md"
+        toggled={theme}
+        toggle={setTheme}
+      />
 
       <Helmet>
-        <meta name="theme-color" content={theme === "dark" && "#1C1917"} />
+        <meta name="theme-color" content={theme && "#1C1917"} />
         <script
           data-goatcounter="https://saketh.goatcounter.com/count"
           async
