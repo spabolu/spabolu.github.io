a041820c585ebcc2e63815de722aee9a6a00bd82
Saketh Pabolu <51687055+spabolu@users.noreply.github.com>
2023-05-30 22:45:47 +0100

    changed courseer card and updated user count

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -8,7 +8,7 @@ export const TimelineItems = [
     title: "Creator of Courseer",
     duration: "Current",
     details:
-      "Currently working on Courseer, a website with 200+ users helping students enroll in classes they need.",
+      "Currently working on Courseer, a website with 250+ users helping students enroll in classes they need.",
   },
   {
     year: "2021",
